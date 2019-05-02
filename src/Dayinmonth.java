4798ab8e5602407564edeb664be637df09d6dc36
khuongduy1484 <khuongduy1484@gmail.com>
2019-05-02 21:49:38 +0700

    commit

--- src/Dayinmonth.java
+++ src/Dayinmonth.java
@@ -16,13 +16,13 @@ public class Dayinmonth {
             case 8 :
             case 10:
             case 12:
-                System.out.println("the month has 31 days");
+                System.out.println("the month " + month +" has 31 days");
                 break;
             case 4:
             case 6:
             case 9:
             case 11:
-                System.out.println("the moth has 30 days");
+                System.out.println("the month " + month +" has 30 days ");
                 break;
 
         }
